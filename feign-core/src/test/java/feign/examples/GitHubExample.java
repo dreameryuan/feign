5d2d2996b603467370a15de3438ddcd98cba8a28
adriancole <adrian.f.cole@gmail.com>
2013-07-11 20:49:23 -0700

    added observer example code

--- feign-core/src/test/java/feign/examples/GitHubExample.java
+++ feign-core/src/test/java/feign/examples/GitHubExample.java
@@ -15,10 +15,10 @@
  */
 package feign.examples;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -32,8 +32,10 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import feign.Feign;
+import feign.Observer;
 import feign.RequestLine;
 import feign.codec.Decoder;
+import feign.codec.ObserverDecoder;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
@@ -47,6 +49,9 @@ public class GitHubExample {
   interface GitHub {
     @RequestLine("GET /repos/{owner}/{repo}/contributors")
     List<Contributor> contributors(@Named("owner") String owner, @Named("repo") String repo);
+
+    @RequestLine("GET /repos/{owner}/{repo}/contributors")
+    void contributors(@Named("owner") String owner, @Named("repo") String repo, Observer<Contributor> contributors);
   }
 
   static class Contributor {
@@ -62,6 +67,25 @@ public class GitHubExample {
     for (Contributor contributor : contributors) {
       System.out.println(contributor.login + " (" + contributor.contributions + ")");
     }
+
+    // example of async processing
+    Observer<Contributor> printlnObserver = new Observer<Contributor>() {
+
+      public int count;
+
+      @Override public void onNext(Contributor element) {
+        count++;
+      }
+
+      @Override public void onSuccess() {
+        System.out.println("found " + count + " contributors");
+      }
+
+      @Override public void onFailure(Throwable cause) {
+        cause.printStackTrace();
+      }
+    };
+    github.contributors("netflix", "feign", printlnObserver);
   }
 
   /**
@@ -80,6 +104,23 @@ public class GitHubExample {
         return gson.fromJson(reader, type);
       }
     };
+
+    @Provides @Singleton Map<String, ObserverDecoder> observerDecoders() {
+      return ImmutableMap.of("GitHub", jsonObserverDecoder);
+    }
+
+    final ObserverDecoder jsonObserverDecoder = new ObserverDecoder<Object>() {
+      Gson gson = new Gson();
+
+      @Override public void decode(Reader reader, Type type, Observer<? super Object> observer) throws IOException {
+        JsonReader jsonReader = new JsonReader(reader);
+        jsonReader.beginArray();
+        while (jsonReader.hasNext()) {
+          observer.onNext(gson.fromJson(jsonReader, type));
+        }
+        jsonReader.endArray();
+      }
+    };
   }
 
   /**
@@ -94,7 +135,7 @@ public class GitHubExample {
     final Decoder jsonDecoder = new Decoder() {
       ObjectMapper mapper = new ObjectMapper().disable(FAIL_ON_UNKNOWN_PROPERTIES).setVisibility(FIELD, ANY);
 
-      @Override public Object decode(Reader reader, final Type type) throws JsonProcessingException, IOException {
+      @Override public Object decode(Reader reader, Type type) throws IOException {
         return mapper.readValue(reader, mapper.constructType(type));
       }
     };
